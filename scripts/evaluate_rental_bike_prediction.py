611250adfc237d98b7148d020aec75b5893338cd
Elaine Chu <chuelaine23@gmail.com>
2024-12-05 20:18:35 -0800

    fixed evaluation figure method

--- scripts/evaluate_rental_bike_prediction.py
+++ scripts/evaluate_rental_bike_prediction.py
@@ -11,21 +11,21 @@ from sklearn import set_config
 from sklearn.metrics import PredictionErrorDisplay
 
 @click.command()
-@click.option('--scaled-test-data', type=str, help="Path to scaled test data")
+@click.option('--test-data', type=str, help="Path to test data")
 # @click.option('--columns-to-drop', type=str, help="Optional: columns to drop")
 @click.option('--pipeline-from-ridge', type=str, help="Path to directory where the ridge fit pipeline object lives")
 @click.option('--pipeline-from-tree', type=str, help="Path to directory where the tree fit pipeline object lives")
 @click.option('--results-to', type=str, help="Path to directory where the plot will be written to")
 @click.option('--seed', type=int, help="Random seed", default=123)
 @click.option('--plot_to', type=str, help="Path to directory where the plots will be written to")
-def main(scaled_test_data, plot_to, pipeline_from_ridge,pipeline_from_tree,results_to, seed):
+def main(test_data, plot_to, pipeline_from_ridge,pipeline_from_tree,results_to, seed):
     '''Evaluates the rental bike regressor on the test data 
     and saves the evaluation results.'''
     np.random.seed(seed)
     set_config(transform_output="pandas")
 
     # read in data & cancer_fit (pipeline object)
-    rental_bike_test = pd.read_csv(scaled_test_data)
+    rental_bike_test = pd.read_csv(test_data)
     # if columns_to_drop:
     #     to_drop = pd.read_csv(columns_to_drop).feats_to_drop.tolist()
     #     rental_bike_test = rental_bike_test.drop(columns=to_drop)
@@ -57,8 +57,7 @@ def main(scaled_test_data, plot_to, pipeline_from_ridge,pipeline_from_tree,resul
         scatter_kwargs={'alpha': 0.12, 's': 10}
     )
 
-    plot_pred_ridge.save(os.path.join(plot_to, "prediction_error_ridge.png"),
-                          scale_factor=2.0)
+    plot_pred_ridge.figure_.savefig(os.path.join(plot_to, "prediction_error_ridge.png"))
     
 
     # Plotting scatter plot for predicted value vs actual value on test set
@@ -70,8 +69,7 @@ def main(scaled_test_data, plot_to, pipeline_from_ridge,pipeline_from_tree,resul
         scatter_kwargs={'alpha': 0.12, 's': 10}
     )
 
-    plot_pred_tree.save(os.path.join(plot_to, "prediction_error_tree.png"),
-                          scale_factor=2.0)
+    plot_pred_tree.figure_.savefig(os.path.join(plot_to, "prediction_error_tree.png"))
     
 
 
